32ebae16fb70487073b6b3424caf9b81fccbe7bf
Alessandro Guarita <alessandro.guarita@al.go.leg.br>
2023-02-01 15:32:46 -0300

    Criando a página de flip

---
+++ torchlight-infinity/js/flip.js
@@ -0,0 +1,13 @@
+// variáveis iniciais
+const taxa = 8
+let compraFe = document.getElementById('compra-fe').value
+let vendaFe = document.getElementById('venda-fe')
+let compraInsumo = document.getElementById('compra-insumo')
+let vendaInsumo = document.getElementById('venda-insumo')
+let resultado = document.querySelector('.resultado')
+
+console.log(resultado)
+
+let teste = document.createElement('p')
+teste.innerHTML = `TESTE`
+resultado.appendChild(teste)
